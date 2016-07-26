e771805a8a726ec8b07637edc8d0e6f64eaf31c8
Sona Mikayelyan <sonamikayleyan@workfront.com>
2016-07-26 16:45:46 +0400

    email validation in UserManager class

--- src/main/java/com/workfront/internship/booklibrary/business/UserManagerImpl.java
+++ src/main/java/com/workfront/internship/booklibrary/business/UserManagerImpl.java
@@ -8,6 +8,7 @@ import com.workfront.internship.booklibrary.dao.UserDAOImpl;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.regex.*;
 
 /**
  * Created by ${Sona} on 7/21/2016.
@@ -15,6 +16,12 @@ import java.security.NoSuchAlgorithmException;
 public class UserManagerImpl implements UserManager {
     private UserDAO userDAO;
     private DataSource dataSource;
+
+    private Pattern pattern;
+    private Matcher matcher;
+
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
     public UserManagerImpl(DataSource dataSource) throws Exception {
         this.dataSource = dataSource;
@@ -29,15 +36,18 @@ public class UserManagerImpl implements UserManager {
      */
     @Override
     public int registration(User user) {
-        if(user != null){
-            if(user.getUsername() != null && user.geteMail() != null &&
-                    user.getName() != null && user.getSurname() != null && user.getPassword() != null &&
-                    user.getAddress() != null && user.getPhone() != null && user.getAccessPrivilege() != null){
-                user.setPassword(getHashedPassword(user.getPassword()));
-                userDAO.add(user);
-                // todo send message to user's email asking to confirm the registration
-                return user.getId();
+        if(user != null) {
+            if (emailFormatValidator(user.geteMail())) {
+                if (user.getUsername() != null && user.geteMail() != null &&
+                        user.getName() != null && user.getSurname() != null && user.getPassword() != null &&
+                        user.getAddress() != null && user.getPhone() != null && user.getAccessPrivilege() != null) {
+                    user.setPassword(getHashedPassword(user.getPassword()));
+                    userDAO.add(user);
+                    // todo send message to user's email asking to confirm the registration
+                    return user.getId();
+                }
             }
+
         }
         return 0;
     }
@@ -119,7 +129,7 @@ public class UserManagerImpl implements UserManager {
     }
 
     private String getHashedPassword(String password){
-        return hashString(password, "SHA-1");
+        return hashString(password, "SHA-256");
     }
 
     private String hashString(String message, String algorithm) {
@@ -140,4 +150,85 @@ public class UserManagerImpl implements UserManager {
         }
         return stringBuffer.toString();
     }
+
+    private boolean emailFormatValidator(String email){
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+
+
+/**
+ import static org.junit.Assert.*;
+ import java.util.Arrays;
+ import java.util.Collection;
+ import org.junit.BeforeClass;
+ import org.junit.Test;
+ import org.junit.runner.RunWith;
+ import org.junit.runners.Parameterized;
+ import org.junit.runners.Parameterized.Parameters;
+
+  @RunWith(Parameterized.class)
+
+    public class EmailFormatValidatorTest {
+
+        private String arg;
+        private static EmailFormatValidator emailFormatValidator;//
+        private Boolean expectedValidation;
+
+        public EmailFormatValidatorTest(String str, Boolean expectedValidation) {//
+            this.arg = str;//
+            this.expectedValidation = expectedValidation;//
+        }
+
+
+        @BeforeClass//
+        public static void initialize() {//
+            emailFormatValidator = new EmailFormatValidator();//
+        }
+
+
+
+        @Parameters//
+        public static Collection<Object[]> data() {
+
+            Object[][] data = new Object[][] {
+
+                    { "javacodegeeks@gmail.com.2j",false },    // it's not allowed to have a digit in the second level tld
+
+                    { "java@java@oracle.com", false },         // you cannot have @ twice in the address
+
+                    { "java!!!@example.com", false },          // you cannot the have special character '!' in the address
+
+                    { "mysite@.com", false },                  // tld cannot start with a dot
+
+                    { "javacodegees.com", false },             // must contain a @ character and a tld
+
+                    { ".javacodegees.com@at.com", false },     // the address cannot start with a dot
+
+                    { "javacodegees..javacom@at.com", false }, // you cannot have double dots in your address
+
+                    { "javacodegeeks@gmail.com",true },
+
+                    { "nikos+mylist@gmail.com", true },
+
+                    { "abc.efg-900@gmail-list.com", true },
+
+                    { "abc123@example.com.gr", true } };
+
+            return Arrays.asList(data);
+
+        }
+
+        @Test
+        public void test() {
+            Boolean res = emailFormatValidator.validate(this.arg);
+            String validv = (res) ? "valid" : "invalid";
+            System.out.println("Hex Color Code "+arg+ " is " + validv);
+            assertEquals("Result", this.expectedValidation, res);
+        }
+    }
+*/
+
 }
